3ed818ee33e4c0260868aec3ba7921bf8c16655e
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-08-20 21:07:25 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ wget https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_li
 gzip --decompress chisel_1.7.6_linux_amd64.gz
 mv chisel_1.7.6_linux_amd64 chisel
 chmod +x chisel
-./chisel server --port $PORT --proxy http://github.com
+./chisel server --port $PORT --proxy https//github.com
